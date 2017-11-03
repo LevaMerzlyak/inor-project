ca56a02448edecdd0ea029203aa8e592693d5482
LevaMerzlyak <13dir13@gmail.com>
2017-11-03 09:19:11 +0300

    add videoPlay.js

---
+++ js/videoPlay.js
@@ -0,0 +1,35 @@
+function VideoStart(sSelector) {
+	var  v = this
+		,mainClass  = 'play__start'
+		;
+
+	v.container = $(sSelector);
+	v.playBtn 	= v.container.find(".playBtn");
+	v.closeBtn	= v.container.find(".close__btn");
+	v.player	= v.container.find("#player");
+
+
+	
+	v.startVideo = function(event) {
+		
+		event.preventDefault();
+		v.container.addClass(mainClass);
+		setTimeout(function() {
+			v.player.attr("src", "https://www.youtube.com/embed//PE-QOjc1Z5w?autoplay=1&showinfo=0&controls=0");
+		}, 1000);
+
+
+	}
+	
+	v.stopVideo = function() {
+
+		v.container.removeClass(mainClass);
+		v.player.attr("src", "");
+
+	}
+	
+	
+
+	v.playBtn.click(v.startVideo);
+	v.closeBtn.click(v.stopVideo);
+}
