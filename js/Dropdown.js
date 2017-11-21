9a0d68ab16d3df2ba01d0cb65162ac6004872f81
LevaMerzlyak <13dir13@gmail.com>
2017-11-21 11:56:03 +0300

    add dropDown.js

---
+++ js/Dropdown.js
@@ -0,0 +1,37 @@
+function DropDown(sSelector) {
+
+	var d = this;
+
+	var opened = 'nav__dropdown_opened';
+
+	d.item = $(sSelector);
+	d.link = d.item.find('.nav__link');
+	d.drowdown = d.item.find('.nav__dropdown');
+
+	d.showHide = function (e) {
+		e.preventDefault();
+		if (d.drowdown.hasClass(opened)) {
+
+			d.hide();
+
+		} else {
+
+			d.show();
+		}
+		
+	}
+	d.hide = function () {
+
+		d.drowdown.removeClass(opened);
+
+	}
+	d.show = function () {
+
+		$('.nav').find('.nav__dropdown_opened').removeClass(opened);
+		d.drowdown.addClass(opened);
+
+	}
+
+	d.link.click(d.showHide);
+
+}
