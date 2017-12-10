d49f1c02da35075b6bcf28fa728a8369de93c13f
LevaMerzlyak <13dir13@gmail.com>
2017-12-11 00:31:38 +0300

    edit vacancy page

--- js/showDisc.js
+++ js/showDisc.js
@@ -4,16 +4,32 @@ function ShowDisc(sSelector) {
 
 	s.object = $(sSelector);
 	s.moreBtn = s.object.find('.vacancy__infoBtn');
-	s.disc = s.object.find('.vacancyDisc');
+	s.disc = $('.vacancyDisc');
 	s.closeBtn = s.disc.find('.vacancyDisc__closeBtn');
+
+	s.t = 0.15;
+	s.pos = 0;
+
+	s.getPosition = function () { 
+		s.t = ($(window).width() <= 770 && $(window).width() < $(window).height()) ? 0.08 : 0.15;
+		s.updatePosition();
+	}
+	s.updatePosition = function () {
+		s.pos = $(document).scrollTop() + ($(window).height() * s.t);
+		s.disc.css('top', s.pos);
+	}
 
 	s.moreBtn.click(function (event) {
 		event.preventDefault();
-		s.disc.show();
+		console.log("btn");
+		s.updatePosition();
+		s.disc.show();		
 	});
 	s.closeBtn.click(function (event) {
 		event.preventDefault();
 		s.disc.hide();
 	});
+	$(window).resize(s.getPosition);
+	$(document).ready(s.getPosition);
 
 }
