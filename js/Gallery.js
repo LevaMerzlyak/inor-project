d3dd27ce22fa4492f0ef85e5afb2e20e07a94c0a
LevaMerzlyak <13dir13@gmail.com>
2017-12-11 17:42:24 +0300

    fix fullscreen for ios

--- js/Gallery.js
+++ js/Gallery.js
@@ -120,41 +120,41 @@ function Gallery(sSelector) {
 		PSV.setPanorama(src);
 		
 	}
-	
 
+	
 	$(document).ready(g.pagInit);
 	g.pag.click(g.showPreview);
-	g.fullScreen.on('click', function(){
-	  // if already full screen; exit
-	  // else go fullscreen
-	  if (
-	    document.fullscreenElement ||
-	    document.webkitFullscreenElement ||
-	    document.mozFullScreenElement ||
-	    document.msFullscreenElement
-	  ) {
-	    if (document.exitFullscreen) {
-	      document.exitFullscreen();
-	    } else if (document.mozCancelFullScreen) {
-	      document.mozCancelFullScreen();
-	    } else if (document.webkitExitFullscreen) {
-	      document.webkitExitFullscreen();
-	    } else if (document.msExitFullscreen) {
-	      document.msExitFullscreen();
-	    }
-	  } else {
-	    element = $('.gallery__wrap').get(0);
-	    if (element.requestFullscreen) {
-	      element.requestFullscreen();
-	    } else if (element.mozRequestFullScreen) {
-	      element.mozRequestFullScreen();
-	    } else if (element.webkitRequestFullscreen) {
-	      element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-	    } else if (element.msRequestFullscreen) {
-	      element.msRequestFullscreen();
-	    }
-	  }
-	  g.fullScreen.toggleClass('fullScreen__btn_out');
+	g.fullScreen.on('click', function() {
+		// if already full screen; exit
+		// else go fullscreen
+		if (
+			document.fullscreenElement ||
+			document.webkitFullscreenElement ||
+			document.mozFullScreenElement ||
+			document.msFullscreenElement
+		) {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+		}
+		} else {
+			element = $('.gallery__wrap').get(0);
+			if (element.requestFullscreen) {
+				element.requestFullscreen();
+			} else if (element.mozRequestFullScreen) {
+				element.mozRequestFullScreen();
+			} else if (element.webkitRequestFullscreen) {
+				element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+			} else if (element.msRequestFullscreen) {
+				element.msRequestFullscreen();
+			}
+		}
+		g.fullScreen.toggleClass('fullScreen__btn_out');
 	});
 
 }
