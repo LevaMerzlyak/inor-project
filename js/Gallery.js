74b98dc9832665b7f371abd96f701bbf84989ab4
LevaMerzlyak <13dir13@gmail.com>
2017-12-11 14:51:33 +0300

    add gallery with 360 photos

--- js/Gallery.js
+++ js/Gallery.js
@@ -9,11 +9,42 @@ function Gallery(sSelector) {
 	g.img = g.current.find('.gallery__img');
 	g.preview = g.gallery.find('.gallery__preview');
 	g.imgPreview = g.preview.find('.preview__img');
+	g.imgNav = g.gallery.find('.gallery__nav');
+	g.fullScreen = g.imgNav.find('.fullScreen__btn');
+
+	g.pans = g.gallery.find('.galleryPag__item_360');
+
+	var src = $(g.pans[0]).find('.gallery__img').attr('src');
+
+	// Panorama display
+	var div = document.getElementById('preview');
+	
+	var PSV = new PhotoSphereViewer({
+		container: 'preview',
+		panorama: src,
+		autoload: false,
+		navbar: [
+    		'autorotate',
+    		'zoom',
+    		'caption',
+    		'fullscreen'
+		]
+	});
 
 
 	g.pagInit = function () {
 
-		g.showImage(g.current);
+		if(g.current.hasClass('galleryPag__item_360')) {
+
+			g.show360(g.current);
+			
+		} else {
+
+			$('.psv-container').hide();			
+			g.showImage(g.current);
+			
+		}
+
 		
 		g.pag.each(function () {
 			
@@ -27,16 +58,13 @@ function Gallery(sSelector) {
 
 	g.showPreview = function (event) {
 		
-		var current = g.gallery.find('.galleryPag__item_current'),
-			target = $(event.target)
-			;
+		var target = $(event.target);
 
-		//console.log(target.attr('class'));
 
-		if (!(target.hasClass('galleryPag__item_current'))) {
-			current.removeClass('galleryPag__item_current');
-			target.addClass('galleryPag__item_current');
+		if (!(target.hasClass('galleryPag__item_360'))) {
 			g.showImage(target);
+		} else {
+			g.show360(target);
 		}
 
 
@@ -44,75 +72,89 @@ function Gallery(sSelector) {
 
 	g.showImage = function (pag) {
 
-		if(!(pag.hasClass('galleryPag__item_360'))) {
+		var current = g.gallery.find('.galleryPag__item_current');
 
-			var img = pag.find('.gallery__img')
+		if(current.hasClass('galleryPag__item_360')) {
+
+			$('.psv-container').hide();			
+			
+		}
+		current.removeClass('galleryPag__item_current');
+		pag.addClass('galleryPag__item_current');
+
+
+		var img = pag.find('.gallery__img')
 				,src = img.attr('src')
 				,w = img.get(0).naturalWidth
 				,h = img.get(0).naturalHeight
 				;
 
-			if ((w / h) > 1) {
-				g.imgPreview.css({'width': '100%','height': 'auto'});
-			} else {
-				g.imgPreview.css({'height': '100%','width': 'auto'});				
-			}
-
-			g.imgPreview.attr('src', src);
-			
+		if ((w / h) > 1) {
+			g.imgPreview.css({'width': '100%','height': 'auto'});
 		} else {
-
-			g.show360(pag);
-
+			g.imgPreview.css({'height': '100%','width': 'auto'});				
 		}
 
-		g.show360 = function (pag) {
+		g.imgNav.show();
+		g.imgPreview.attr('src', src);
+
+	}
+
+	g.show360 = function (pag) {
+
+		var current = g.gallery.find('.galleryPag__item_current');
+
+		if(!(current.hasClass('galleryPag__item_360'))) {
+
+			$('.psv-container').show();		
 			
-			var src = pag.find('.gallery__img').attr('src');
-
-			g.imgPreview.attr('src', '');
-
-			// Loader
-			var loader = document.createElement('div');
-			loader.className = 'loader';
-
-			// Panorama display
-			var div = document.getElementById('preview');
-			//div.style.height = '30px';
-			
-			var PSV = new PhotoSphereViewer({
-				// Panorama, given in base 64
-				panorama: src,
-
-				// Container
-				container: div,
-
-				// Deactivate the animation
-				time_anim: 3000,
-
-				// Display the navigation bar
-				navbar: true,
-
-				// Resize the panorama
-				size: {
-					width: '100%',
-					height: '100%'
-				},
-
-				// HTML loader
-				loading_html: loader,
-
-				// No XMP data
-				usexmpdata: true
-			});
-
 		}
+		current.removeClass('galleryPag__item_current');
+		pag.addClass('galleryPag__item_current');
+			
+		var src = pag.find('.gallery__img').attr('src');
 
+		g.imgPreview.attr('src', '');
+		g.imgNav.hide();
 
+		PSV.setPanorama(src);
+		
 	}
 	
 
 	$(document).ready(g.pagInit);
 	g.pag.click(g.showPreview);
+	g.fullScreen.on('click', function(){
+	  // if already full screen; exit
+	  // else go fullscreen
+	  if (
+	    document.fullscreenElement ||
+	    document.webkitFullscreenElement ||
+	    document.mozFullScreenElement ||
+	    document.msFullscreenElement
+	  ) {
+	    if (document.exitFullscreen) {
+	      document.exitFullscreen();
+	    } else if (document.mozCancelFullScreen) {
+	      document.mozCancelFullScreen();
+	    } else if (document.webkitExitFullscreen) {
+	      document.webkitExitFullscreen();
+	    } else if (document.msExitFullscreen) {
+	      document.msExitFullscreen();
+	    }
+	  } else {
+	    element = $('.gallery__wrap').get(0);
+	    if (element.requestFullscreen) {
+	      element.requestFullscreen();
+	    } else if (element.mozRequestFullScreen) {
+	      element.mozRequestFullScreen();
+	    } else if (element.webkitRequestFullscreen) {
+	      element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+	    } else if (element.msRequestFullscreen) {
+	      element.msRequestFullscreen();
+	    }
+	  }
+	  g.fullScreen.toggleClass('fullScreen__btn_out');
+	});
 
 }
